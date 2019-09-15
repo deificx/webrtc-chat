7b9a141d0821addaa7b98e0dc86fd6e353b95141
Even Alander <even@alander.org>
2019-09-15 14:02:51 +0200

    split out Message to own component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {EditMessage} from './EditMessage';
 import {MessageInput} from './MessageInput';
 import {RoomState, RTCChatMessage} from '../types';
+import {Message} from './Message';
 import styled from 'styled-components';
 
 const Div = styled.div`
@@ -15,26 +15,6 @@ const Div = styled.div`
     width: 100%;
 `;
 
-const Message = styled.article`
-    padding: 0 25px 10px;
-`;
-
-const Time = styled.time`
-    color: #9a9fa8;
-    display: inline-block;
-    padding: 0 5px;
-`;
-
-const Section = styled.section`
-    margin: 5px 0;
-`;
-
-const Edit = styled.span`
-    color: grey;
-    display: inline-block;
-    padding: 0 0.5em;
-`;
-
 export const Chat: React.FC<{
     state: RoomState;
     editMessage: (old: RTCChatMessage, message: string) => void;
@@ -43,28 +23,9 @@ export const Chat: React.FC<{
 }> = ({state, editMessage, handleEdit, sendMessage}) => {
     return (
         <Div>
-            {state.messages.map(message => {
-                const time = new Date(message.timestamp);
-                const author = state.authors.find(a => a.id === message.author.id);
-                return (
-                    <Message key={message.id}>
-                        <header>
-                            {author ? <strong>{author.displayName}</strong> : <del>{message.author.displayName}</del>}
-                            <Time dateTime={time.toISOString()}>{time.toLocaleString()}</Time>
-                        </header>
-                        {message.edited && message.message === '' ? (
-                            <del>message deleted</del>
-                        ) : state.editing === message.id ? (
-                            <EditMessage message={message} editMessage={editMessage} />
-                        ) : (
-                            <>
-                                <Section onDoubleClick={() => handleEdit(message.id)}>{message.message}</Section>
-                                {message.edited && <Edit>(edited)</Edit>}
-                            </>
-                        )}
-                    </Message>
-                );
-            })}
+            {state.messages.map(message => (
+                <Message message={message} state={state} editMessage={editMessage} handleEdit={handleEdit} />
+            ))}
             <MessageInput sendMessage={sendMessage} />
         </Div>
     );
---
+++ src/components/Message.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {EditMessage} from './EditMessage';
+import {RoomState, RTCChatMessage} from '../types';
+import styled from 'styled-components';
+
+const Article = styled.article`
+    padding: 0 25px 10px;
+`;
+
+const Time = styled.time`
+    color: #9a9fa8;
+    display: inline-block;
+    padding: 0 5px;
+`;
+
+const Section = styled.section`
+    margin: 5px 0;
+`;
+
+const Edit = styled.span`
+    color: grey;
+    display: inline-block;
+    padding: 0 0.5em;
+`;
+
+export const Message: React.FC<{
+    message: RTCChatMessage;
+    state: RoomState;
+    editMessage: (old: RTCChatMessage, message: string) => void;
+    handleEdit: (id: string) => void;
+}> = ({message, state, editMessage, handleEdit}) => {
+    const time = new Date(message.timestamp);
+    const author = state.authors.find(a => a.id === message.author.id);
+    return (
+        <Article key={message.id}>
+            <header>
+                {author ? <strong>{author.displayName}</strong> : <del>{message.author.displayName}</del>}
+                <Time dateTime={time.toISOString()}>{time.toLocaleString()}</Time>
+            </header>
+            {message.edited && message.message === '' ? (
+                <del>message deleted</del>
+            ) : state.editing === message.id ? (
+                <EditMessage message={message} editMessage={editMessage} />
+            ) : (
+                <>
+                    <Section onDoubleClick={() => handleEdit(message.id)}>{message.message}</Section>
+                    {message.edited && <Edit>(edited)</Edit>}
+                </>
+            )}
+        </Article>
+    );
+};
